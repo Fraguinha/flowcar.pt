aleph-two/flowcar

feat: Add webapp

webapp/src/components/mod.rs
@@ -1,0 +1,8 @@
+pub mod card;
+pub mod carousel;
+pub mod feature;
+pub mod footer;
+pub mod list;
+pub mod nav;
+pub mod pagination;
+pub mod summary;

Fraguinha/flowcar.pt

webapp/src/components/mod.rs
@@ -1,0 +1,10 @@
+pub mod card;
+pub mod details;
+pub mod error;
+pub mod feature;
+pub mod footer;
+pub mod gallery;
+pub mod list;
+pub mod nav;
+pub mod pagination;
+pub mod summary;