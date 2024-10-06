aleph-two/flowcar

feat: Add webapp

webapp/src/lib.rs
@@ -1,0 +1,17 @@
+pub mod app;
+pub mod components;
+#[cfg(feature = "ssr")]
+pub mod database;
+pub mod error;
+pub mod models;
+pub mod pages;
+#[cfg(feature = "ssr")]
+pub mod server;
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(App);
+}

Fraguinha/flowcar.pt

webapp/src/lib.rs
@@ -1,0 +1,19 @@
+pub mod app;
+pub mod auth;
+pub mod components;
+#[cfg(feature = "ssr")]
+pub mod database;
+pub mod models;
+pub mod pages;
+#[cfg(feature = "ssr")]
+pub mod server;
+#[cfg(feature = "ssr")]
+pub mod state;
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(App);
+}